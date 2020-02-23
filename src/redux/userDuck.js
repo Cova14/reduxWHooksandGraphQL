1b77a4002a41cba8f5bc6a8a734533a96708f4dd
Cova14 <arty1498@hotmail.com>
2020-02-23 13:22:45 -0600

    Created first duck

---
+++ src/redux/userDuck.js
@@ -0,0 +1,14 @@
+// constants
+let initialData = {
+  loggedIn: false
+}
+let LOGIN = 'LOGIN'
+// reducer
+export default function reducer(state = initialData, action) {
+  switch(action.type) {
+    case LOGIN:
+    default:
+      return state
+  }
+}
+// action (action creator)
