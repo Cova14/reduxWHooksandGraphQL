56acd2e5c5955247560896f1cc47014cef245180
Cova14 <arty1498@hotmail.com>
2020-02-23 15:22:53 -0600

    Using connect in HomePage to pass store as props

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,46 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Card from '../card/Card'
 import styles from './home.module.css'
-import axios from 'axios'
+import { connect } from 'react-redux'
 
-let URL = "https://rickandmortyapi.com/api"
+function Home({chars}) {
 
-export default function Home() {
+	function renderCharacter() {
+		let char = chars[0]
+		return (
+			<Card {...char} />
+		)
+	}
 
-    let [chars, setChars] = useState([])
-
-    useEffect(() => {
-        getCharacters()
-    }, [])
-
-    function nextChar() {
-        chars.shift()
-        if (!chars.length) {
-            //get more characters
-        }
-        setChars([...chars])
-    }
-
-    function renderCharacter() {
-        let char = chars[0]
-        return (
-            <Card leftClick={nextChar} {...char} />
-        )
-    }
-
-    function getCharacters() {
-        return axios.get(`${URL}/character`)
-            .then(res => {
-                setChars(res.data.results)
-            })
-    }
-
-    return (
-        <div className={styles.container}>
-            <h2>Personajes de Rick y Morty</h2>
-            <div>
-                {renderCharacter()}
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.container}>
+			<h2>Personajes de Rick y Morty</h2>
+			<div>
+				{renderCharacter()}
+			</div>
+		</div>
+	)
 }
+
+function mapState(state) {
+	return {
+		chars: state.chars.array
+	}
+}
+
+export default connect(mapState)(Home)
