74771eb5f72e429bbc873e164262a03606c5f4dd
Cova14 <arty1498@hotmail.com>
2020-02-23 14:55:01 -0600

    Added charsReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import userReducer from './userDuck'
+import charsReducer from './charsDucks'
 import thunk from 'redux-thunk'
 
 let rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  chars: charsReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
