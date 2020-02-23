06ce54ba0aa352dfb1d929ec86e7fd3297b2862c
Cova14 <arty1498@hotmail.com>
2020-02-23 15:08:19 -0600

    Created all cases with axios on chars reducer

--- src/redux/charsDucks.js
+++ src/redux/charsDucks.js
@@ -17,22 +17,42 @@ let GET_CHARACTERS_ERROR = 'GET_CHARACTERS_ERROR'
 export default function reducer(state = initialData, action) {
   switch (action.type) {
     case 'GET_CHARACTERS':
+      return {
+        ...state, fetching: true
+      }
     case 'GET_CHARACTERS_SUCCESS':
       return {
-        ...state, array: action.payload
+        ...state,
+        array: action.payload,
+        fetching: false
       }
       case 'GET_CHARACTERS_ERROR':
+        return{
+          ...state,
+          fetching: false,
+          error: action.payload
+        }
       default:
         return state
   }
 }
 // actions 
 export let getCharactersAction = () => (dispatch, getState) => {
+  dispatch({
+    type: GET_CHARACTERS
+  })
   return axios.get(URL)
     .then(res => {
       dispatch({
         type: GET_CHARACTERS_SUCCESS,
         payload: res.data.results
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      dispatch({
+        type: GET_CHARACTERS_ERROR,
+        payload: err.response.message
       })
     })
 }
