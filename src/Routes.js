90603debeda52ea50c04f751370969d71e52b1f1
Cova14 <arty1498@hotmail.com>
2020-02-23 17:36:52 -0600

    Added functionality to check loggin with redux (connect) instead of localstorage

--- src/Routes.js
+++ src/Routes.js
@@ -3,23 +3,32 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from './components/home/HomePage'
 import FavPage from './components/favs/FavPage'
 import LoginPage from './components/login/LoginPage'
+import { connect } from 'react-redux'
 
-function PrivateRoute({path, component, ...rest}) {
-    let storage = localStorage.getItem('storage')
-    storage = JSON.parse(storage)
-    if(storage && storage.user) {
+function PrivateRoute({path, component, loggedIn, ...rest}) {
+    if(loggedIn) {
         return <Route path={path} component={component} {...rest} />
     } else {
         return <Redirect to="/login/" {...rest} />
     }
 }
 
-export default function Routes() {
+function Routes({loggedIn}) {
+
     return (
         <Switch>
-            <PrivateRoute exact path="/" component={Home} />
-            <PrivateRoute path="/favs" component={FavPage} />
+            <PrivateRoute exact path="/" component={Home} loggedIn={loggedIn} />
+            <PrivateRoute path="/favs" component={FavPage} loggedIn={loggedIn} />
             <Route path="/login" component={LoginPage} />
         </Switch>
     )
 }
+
+function mapState({user:{ loggedIn, uid}}) {
+    return {
+        loggedIn,
+        uid
+    }
+}
+
+export default connect(mapState)(Routes)
