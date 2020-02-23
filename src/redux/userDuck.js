2e77bf818da6e55ced9fa9311e3857f103db488c
Cova14 <arty1498@hotmail.com>
2020-02-23 16:16:57 -0600

    Created function on userDuck to change the state when login

--- src/redux/userDuck.js
+++ src/redux/userDuck.js
@@ -1,14 +1,62 @@
+import { loginWithGoogle } from '../firebase'
+
 // constants
 let initialData = {
-  loggedIn: false
+  loggedIn: false,
+  fetching: false
 }
 let LOGIN = 'LOGIN'
+let LOGIN_SUCCES = 'LOGIN_SUCCES'
+let LOGIN_ERROR = 'LOGIN_ERROR'
 // reducer
 export default function reducer(state = initialData, action) {
   switch(action.type) {
     case LOGIN:
+      return {
+        ...state, fetching: true
+      }
+    case LOGIN_SUCCES:
+      return {
+        ...state,
+        fetching: false,
+        ...action.payload
+      }
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      }
     default:
       return state
   }
 }
+
+// aux function
+function saveStorage(storage) {
+localStorage.storage = JSON.stringify(storage)
+}
+
 // action (action creator)
+
+export let doGoogleLoginAction = () => (dispatch, getState) => {
+  dispatch({
+    type: LOGIN
+  })
+  return loginWithGoogle()
+    .then(user => {
+      saveStorage()
+      dispatch({
+        type: LOGIN_SUCCES,
+        payload: {...user}
+      })
+      saveStorage(getState())
+    })
+    .catch(e => {
+      console.log(e)
+      dispatch({
+        type: LOGIN_ERROR,
+        payload: e.message
+      })
+    })
+}
