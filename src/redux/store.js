ac61d3a602d0b1057265c131f3f5fc678a5bee8a
Cova14 <arty1498@hotmail.com>
2020-02-23 15:00:38 -0600

    Dispatching getChars when app starts

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import userReducer from './userDuck'
-import charsReducer from './charsDucks'
+import charsReducer, {getCharactersAction} from './charsDucks'
 import thunk from 'redux-thunk'
 
 let rootReducer = combineReducers({
@@ -15,5 +15,7 @@ export default function generateStore() {
     rootReducer,
     composeEnhancers(applyMiddleware(thunk))
   )
+  // consiguiendo los personajes por primera vez
+  getCharactersAction()(store.dispatch, store.getState)
   return store
 }
